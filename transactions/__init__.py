6181ce9d3532b140d7ff53f7df63647b86d8e86e
Warpiiv <118689834+Warpiiv@users.noreply.github.com>
2025-06-14 13:48:24 +0100

    Update __init__.py

--- transactions/__init__.py
+++ transactions/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-transactions_bp = Blueprint('transactions', __name__)
+transactions_bp = Blueprint('transactions', __name__, template_folder='templates')
 
 from .routes import *
